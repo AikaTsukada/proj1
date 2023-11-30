8ccdc7720c379ac55c4fb5746b7c4ae0d429f1b2
cy21227 <cy21227@shibaura-it.ac.jp>
2023-11-30 14:18:37 +0900

    コミットメッセージ

---
+++ test.c
@@ -0,0 +1,13 @@
+// 2つの整数を入力して、その和を出力するプログラム
+// 基本的なCの書き方、scanf, printfの書き方を思い出すこと
+#include <stdio.h>
+
+int main()
+{
+	int x,y;
+	scanf("%d", &x);
+	scanf("%d", &y);
+	printf("%d\n", x+y);
+	return 0;
+}
+
